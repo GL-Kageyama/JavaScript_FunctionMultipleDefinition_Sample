7dcc567f208daf73b654ab84b3698eb8f5a9fbfe
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-06-20 13:38:00 +0900

    Closure Sample

--- script.js
+++ script.js
@@ -1,53 +1,38 @@
-// Name definition
-let personNames = {
-    values: ["Dennis Ritchie", "Charles Simonyi", "Andy Hopper", "Ken Thompson", "Yukihiro Matsumoto",
-             "James Gosling", "John Resig", "Brendan Eich", "Mark Adler", "Marc Andreessen",
-             "Larry Wall", "Larry Tesler", "Linus Torvalds", "Richard Brodie", "Richard Greenblatt"]
+// Functions that use closures to perform arithmetic operations
+let calcClosure = function() {
+
+    // Closure values can be temporarily saved
+    let closureCounter = 1;
+    let closureNum = 100;
+
+    const fixNum1 = 10;
+    const fixNum2 = 20;
+
+    return function Calculation() {
+
+        console.log("Closure times : " + closureCounter);
+        console.log("Closure value : " + closureNum);
+
+        // Addition
+        closureNum += fixNum2;
+        console.log(closureNum);
+        // Subtraction
+        closureNum -= fixNum1;
+        console.log(closureNum);
+        // Multiplication
+        closureNum *= fixNum2;
+        console.log(closureNum);
+        // Division
+        closureNum /= fixNum1;
+        console.log(closureNum);
+
+        closureCounter++;
+    };
 };
 
-// Methods for multiple definitions of functions
-function addMethod(object, name, fn) {
-    var old = object[name];
-    object[name] = function() {
-        if (fn.length == arguments.length)
-            return fn.apply(this, arguments)
-        else if (typeof old == "function")
-            return old.apply(this, arguments);
-    }
-}
+let calc = calcClosure();
 
-// Create a findProgrammer function (no arguments)
-addMethod(personNames, "findProgrammer", function(){
-    return this.values;
-});
-
-// Create a findProgrammer function (with one argument)
-addMethod(personNames, "findProgrammer", function(name){
-    let ret = [];
-    for (let i = 0; i < this.values.length; i++)
-        if (this.values[i].indexOf(name) == 0)
-            ret.push(this.values[i]);
-    return ret;
-})
-
-// Create a findProgrammer function (with two arguments)
-addMethod(personNames, "findProgrammer", function(first, last){
-    let ret = [];
-    for (let i = 0; i < this.values.length; i++)
-        if (this.values[i] == (first + " " + last))
-            ret.push(this.values[i]);
-    return ret;
-})
-
-console.log("Output all person names");
-console.log(personNames.findProgrammer());
-
-console.log("Output people who match the first name");
-console.log(personNames.findProgrammer("Larry"));
-console.log(personNames.findProgrammer("Richard"));
-console.log(personNames.findProgrammer("Mark"));
-
-console.log("Output the people whose first and last names match");
-console.log(personNames.findProgrammer("Yukihiro", "Matsumoto"));
-console.log(personNames.findProgrammer("Dennis", "Ritchie"));
-console.log(personNames.findProgrammer("Charles", "Simonyi"));
+// Compute using the variables stored by the closure
+calc();
+calc();
+calc();
